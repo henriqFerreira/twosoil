df26b1b3f3a4e7d74116d4870d7b6fe0fa04d1fa
Elbert Jean <elbertjeandossantos@gmail.com>
2024-01-18 15:19:45 -0300

    feat: estrutura da interface geoJson

--- src/components/Map/Map.component.tsx
+++ src/components/Map/Map.component.tsx
@@ -24,20 +24,20 @@ export default function Map() {
 			scrollWheelZoom={true}
 		>
 			<LayersControl>
-				<LayersControl.BaseLayer checked name="ESRI World Imagery">
+				<LayersControl.BaseLayer  name="ESRI World Imagery">
 					<TileLayer
 						attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
 						url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
 					/>
 				</LayersControl.BaseLayer>
-				<LayersControl.BaseLayer name="OpenStreetMap">
+				<LayersControl.BaseLayer checked name="OpenStreetMap">
 					<TileLayer
 						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 					/>
 				</LayersControl.BaseLayer>
+				< AreasSJCLayer data={dataAreasSJC} setGeoFilter={setGeoFilter} getGeoFilter={getGeoFilter} checked={true}/>
 			</LayersControl>
-			< AreasSJCLayer data={dataAreasSJC}  setGeoFilter={setGeoFilter} getGeoFilter={getGeoFilter}/>
 			< DrawMap />
 		</MapContainer>
 	);
